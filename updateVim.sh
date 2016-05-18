a0aa06ff7451be196e00300472baef881cc00602
Chloe Dequeker <ch.dequeker@gmail.com>
2016-05-18 16:31:41 +0200

    update bashrc & updateVim

--- updateVim.sh
+++ updateVim.sh
@@ -4,6 +4,7 @@ git submodule foreach git pull origin master
 rm -rf ~/.vim/ 
 mkdir ~/.vim/
 cp -r vim/* ~/.vim/
+cp vim/.ycm_extra_conf.py ~/.vim/
 cd ~/.vim/bundle/YouCompleteMe/
 ./install.py --clang-completer
 cd $OLDPWD
